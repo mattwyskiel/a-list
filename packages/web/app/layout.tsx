dcc0fd30d873d9989cac7496e124928664e7d136
Matthew Wyskiel <matt@mattwyskiel.com>
2024-02-14 00:48:32 -0500

    Add Google Analytics

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -33,6 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="G-T6FX4D86NV" />
     </html>
   );
 }
