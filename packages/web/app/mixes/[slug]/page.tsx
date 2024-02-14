45996676e1f9b73121712935783ed828c0cc82ed
Matthew Wyskiel <matt@mattwyskiel.com>
2024-02-13 21:00:22 -0500

    Implement player

--- packages/web/app/mixes/[slug]/page.tsx
+++ packages/web/app/mixes/[slug]/page.tsx
@@ -1,5 +1,28 @@
-import { Player } from "@/components/player";
+import { Player, PlayerProps } from "@/components/player";
 
-export default function Page({ params }: { params: { slug: string } }) {
-  return <Player />;
+type Mix = {
+  id: number;
+  title: string;
+  description: string;
+  audioUrl: string;
+  publishDate: string;
+};
+
+async function getData(slug: string): Promise<Mix> {
+  const res = await fetch("https://api.mattwyskiel.com/a-list/?slug=" + slug);
+
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const mix = await getData(params.slug);
+  return <Player mix={mix} />;
 }
