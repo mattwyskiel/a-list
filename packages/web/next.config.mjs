076fde87928a3f9cc68437c7bb30f78666c515e5
Matthew Wyskiel <matt@mattwyskiel.com>
2024-02-13 21:23:12 -0500

    Add webpack configuration for production build

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ["assets.mattwyskiel.com"],
   },
+  webpack: (config, options) => {
+    if (!options.dev) {
+      config.devtool = "source-map";
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
